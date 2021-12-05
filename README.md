938d9583e642ddbbb79cbe020b7118ba8699aab5
Jonas <jonas_sorgenfrei@yahoo.de>
2021-12-05 03:01:38 +0100

    added enable option, added env evalExpression, added exception catch when json file is corrupt

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Plugin to Manage Nuke Plugins using json files without the need to touch any files like the init.py file 
 for testing and using nuke plugins.
 
-
+## Install
 Just download/clone the PluginManager and add the following code to ```<user home directory>/.nuke/init.py```
 
 ```
@@ -17,11 +17,14 @@ pluginManager.init(silent=False)
 
 The silent flag can be used to print the path of the loaded plugin directories at startup (if set to False).
 
-To add plugins just create a new additional json File (like the examples) in the plugin folder.
-The json files needs the following structure:
+## Using
+To add plugins just create a new json File (like the examples) in the plugin folder.
+
+Example json file structure:
 
 ```
 {
+	"enable": true,
 	"env": [
 		{
 			"<NAME_OF_PLUGIN>": "<PATH_TO_PLUGIN>"
@@ -32,3 +35,14 @@ The json files needs the following structure:
 ```
 
 The parsers adds all environment variables env to the environement Variables and adds the plugin (folder) at the plugin_path location.
+
+All keys are optional.
+
+
+| Json Keys  | description |
+| --- | --- |
+| enable  | (boolean/string) Flag if the plugin should be enabled. Can be a simple expression like: nuke.NUKE_VERSION_MAJOR==13 |
+| env  | (string) Environment variables like the path to the plugin or multiple subpathes or other environment variables |
+| plugin_path  | (string) The path to the plugin which should be appended to the nuke plugin pathes |
+
+Note: A definition can be used to only add environment variables on start up. See: examplePlugins/env.json
--- pluginManager.py
+++ pluginManager.py
@@ -4,19 +4,56 @@ import os
 import nuke
 
 def init(silent=True):
+    """
+    Initializes the Plugin Manager and loads the plugin pathes defined by the json files in plugins.
+
+    Args:
+        silent (bool, optional): Flag to print the loaded Plugin pathes on start up. Defaults to True.
+    """
     managerDirectory = os.path.dirname(__file__)
     plugins = os.listdir(os.path.join(managerDirectory, "plugins"))
+
     for plugin in plugins:
         if plugin.endswith(".json"):
-            with open(os.path.join(managerDirectory, "plugins", plugin)) as json_file:
-                data = json.load(json_file)
-                if "env" in data:
-                    for item in data["env"]:
-                        for key in item:
-                            os.environ[key]=item[key]
-                if "plugin_path" in data:
-                    pluginPath = os.path.expandvars(data["plugin_path"])
-                    if os.path.exists(pluginPath):
-                        nuke.pluginAddPath(pluginPath)
-                    if not silent:
-                        print("Loaded Plugin: {}".format(os.path.expandvars(data["plugin_path"])))
+            definitionFile = os.path.join(managerDirectory, "plugins", plugin)
+            with open(definitionFile) as json_file:
+                try:
+                    data = json.load(json_file)
+
+                    # pre checks 
+
+                    # enable / disable plugin flag
+                    if "enable" in data:
+                        enableExpr = data["enable"]
+                        enableExprType = type(data["enable"])
+                        
+                        if enableExprType is bool:
+                            if not enableExpr:
+                                continue
+                        elif enableExprType is str:
+                            # evaluate expressions
+                            if not eval(enableExpr):
+                                continue
+
+                    # load data 
+
+                    # append environment variables
+                    if "env" in data:
+                        for item in data["env"]:
+                            for key in item:
+                                os.environ[key]=os.path.expandvars(item[key])
+
+                    # append plugin pathes
+                    if "plugin_path" in data:
+                        pluginPath = os.path.expandvars(data["plugin_path"])
+                        
+                        if os.path.exists(pluginPath):
+                            # check if plugin path exists
+                            nuke.pluginAddPath(pluginPath)
+                            if not silent:
+                                print("Loaded Plugin: {}".format(pluginPath))
+                        else:
+                            print("Error: Can't load Plugin: {}".format(pluginPath))
+
+                except json.decoder.JSONDecodeError as e:
+                    print("Can't load Plugin-Definition: {}".format(definitionFile))
